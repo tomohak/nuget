3ae82f9ff8baad3f73cf42fb849b6df7374a6eaf
tomohak <tomohak@outlook.com>
2018-12-10 12:30:57 +0900

    Add FontIconControls

---
+++ FontIconControls/FontIconControls/CrossFontIconControls.shared.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Plugin.FontIconControls
+{
+    /// <summary>
+    /// Cross FontIconControls
+    /// </summary>
+    public static class CrossFontIconControls
+    {
+        static Lazy<IFontIconControls> implementation = new Lazy<IFontIconControls>(() => CreateFontIconControls(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        /// <summary>
+        /// Gets if the plugin is supported on the current platform.
+        /// </summary>
+        public static bool IsSupported => implementation.Value == null ? false : true;
+
+        /// <summary>
+        /// Current plugin implementation to use
+        /// </summary>
+        public static IFontIconControls Current
+        {
+            get
+            {
+                IFontIconControls ret = implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+        }
+
+        static IFontIconControls CreateFontIconControls()
+        {
+#if NETSTANDARD1_0 || NETSTANDARD2_0
+            return null;
+#else
+#pragma warning disable IDE0022 // Use expression body for methods
+            return new FontIconControlsImplementation();
+#pragma warning restore IDE0022 // Use expression body for methods
+#endif
+        }
+
+        internal static Exception NotImplementedInReferenceAssembly() =>
+            new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+
+    }
+}
