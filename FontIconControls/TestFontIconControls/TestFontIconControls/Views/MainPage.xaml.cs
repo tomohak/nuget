dd2c352e3ea25c4e2aa30577fd0cfd3410bb9843
tomohak <tomohak@outlook.com>
2018-12-19 12:28:21 +0900

    Add TestFontIconControls

---
+++ FontIconControls/TestFontIconControls/TestFontIconControls/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestFontIconControls.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : Plugin.FontIconControls.FontIconTabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
