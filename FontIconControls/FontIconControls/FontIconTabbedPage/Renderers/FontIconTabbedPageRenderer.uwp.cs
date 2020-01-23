e6d10c85ab7a4f16db9bf846d2c7683f581f5eba
tomohiro <tomohiroakutsu@outlook.com>
2020-01-23 17:12:53 +0900

    Update TestFontIconControls

--- FontIconControls/FontIconControls/FontIconTabbedPage/Renderers/FontIconTabbedPageRenderer.uwp.cs
+++ FontIconControls/FontIconControls/FontIconTabbedPage/Renderers/FontIconTabbedPageRenderer.uwp.cs
@@ -23,7 +23,7 @@ namespace Plugin.FontIconControls.UWP.FontIconTabbedPage.Renderers
                 var xamlStr = @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation' 
                             xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml' 
                             xmlns:local='using:Plugin.FontIconControls' 
-                            xmlns:converters='using:Plugin.FontIconControls.UWP.FontIconTabbedPage.Converters'>
+                            xmlns:converters='using:Plugin.FontIconControls.UWP.Converters'>
                             <StackPanel x:Name='TabbedPageHeaderPanel'>
                                 <StackPanel.Resources>
                                     <converters:FileImageSourceToFontIconConverter x:Key='FileImageSourceToFontIconConverter'/>
